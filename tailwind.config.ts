6e50cdebbe9e8fda8227b3f8dcf16ae4760a92d1
Cody Christ <cchrist1210@gmail.com>
2023-10-12 09:51:22 -0700

    Updated Tailwind configuration / global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+const tailwindBaseConfig = require('@pickleballinc/configs');
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
+  presets: [tailwindBaseConfig],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blue: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
       },
+      fontSize: {
+        sm: '14px',
+        md: '16px',
+      }
     },
+    screens: {
+      sm: { max: "639px" }
+    }
   },
   plugins: [],
+  corePlugins: {
+    preflight: false
+  }
 }
 export default config
