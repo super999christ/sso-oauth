d247386ac43128989ae3e51c7d0909ce4cf93cf5
Cody Christ <cchrist1210@gmail.com>
2023-10-20 03:28:27 -0700

    Fixed Recaptcha v3 issues

--- lib/components/Wrappers/GoogleRecaptchaWrapper.tsx
+++ lib/components/Wrappers/GoogleRecaptchaWrapper.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import type { FC, PropsWithChildren } from 'react';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 const GoogleRecaptchaWrapper: FC<PropsWithChildren> = ({ children }) => {
   return (
     <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY as string}
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string}
     >
       {children}
     </GoogleReCaptchaProvider>
--- lib/server/recaptcha.ts
+++ lib/server/recaptcha.ts
@@ -2,12 +2,9 @@
 
 import axios from 'axios';
 
-export async function verifyCaptcha(token: string | null) {
+export const validateRecaptchaToken = async (token: string) => {
   const res = await axios.post(
     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
   );
-  if (res.data.success) {
-    return 'success!';
-  }
-  throw new Error('Failed Captcha');
-}
+  return res.data.success;
+};
