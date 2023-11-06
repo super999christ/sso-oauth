0f0230cfb30ccab53f5888159953790500951308
Cody Christ <cchrist1210@gmail.com>
2023-11-06 21:23:33 +0100

    add dockerfile and dev git workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withBundleAnalyzer({
+  output: 'standalone',
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: '',
+  reactStrictMode: false,
+  compress: true,
+  experimental: {
+    webpackBuildWorker: true
+  }
+});
 
 module.exports = nextConfig;
