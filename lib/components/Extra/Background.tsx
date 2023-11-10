a2c27e7a49e82f717088a46cb29b4ccc90613193
Cody Christ <cchrist1210@gmail.com>
2023-11-10 01:07:15 +0100

    fix background image cover

--- lib/components/Extra/Background.tsx
+++ lib/components/Extra/Background.tsx
@@ -2,9 +2,12 @@
 
 const Background = () => {
   return (
-    <img
-      src="/images/background.jpg"
+    <div
       className="fixed left-0 top-0 z-[-1] h-[100vh] w-[100vw]"
+      style={{
+        backgroundImage: 'url("/images/background.jpg")',
+        backgroundSize: 'cover'
+      }}
     />
   );
 };
