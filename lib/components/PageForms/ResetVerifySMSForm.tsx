03b088b389cb87c7b5c0032c2c5dc1d43ca07b76
Cody Christ <cchrist1210@gmail.com>
2023-12-05 06:36:39 -0800

    Updated SMS with mobile icon

--- lib/components/PageForms/ResetVerifySMSForm.tsx
+++ lib/components/PageForms/ResetVerifySMSForm.tsx
@@ -104,7 +104,7 @@ export default function ResetVerifySMSForm(props: IFormProps) {
       <div className="flex w-[100vw] flex-col items-center self-start pt-[104px] sm:pt-[60px]">
         <div className="box-border flex w-[540px] flex-col items-center rounded-[12px] bg-white px-5 pb-12 pt-8 text-center sm:h-full sm:w-full sm:max-w-[420px] sm:px-4 sm:pb-4">
           <div className="flex justify-center gap-6">
-            <img src="/icons/icon-phone.svg" width={64} height={64} />
+            <img src="/icons/icon-mobile.svg" width={64} height={64} />
           </div>
           <div className="mt-6 text-[30px] font-semibold leading-9 sm:text-[24px]">
             Text sent to
