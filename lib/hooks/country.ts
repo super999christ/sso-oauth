ca00487bc5b3998b25624a4eca0c5f0c084e018d
Cody Christ <cchrist1210@gmail.com>
2023-12-26 16:49:45 -0800

    Removed duplicated countries from the list

--- lib/hooks/country.ts
+++ lib/hooks/country.ts
@@ -6,9 +6,6 @@ export const getCountries = async () => {
   const response = await axios.get<{ results: Country[] }>(
     '/api/proxy/v1/data/lookup_countries'
   );
-  const countries = response.data.results;
-  countries.unshift(...countries.filter(item => item.abbreviation === 'CAN'));
-  countries.unshift(...countries.filter(item => item.abbreviation === 'USA'));
   return response.data;
 };
 
