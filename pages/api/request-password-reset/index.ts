cc649ed07747d68b7cfa11b359fd2dfe1e1c52fc
Cody Christ <cchrist1210@gmail.com>
2024-06-06 08:25:50 -0700

    Added pb-device / pb-user-ip params for /request-password-reset API

--- pages/api/request-password-reset/index.ts
+++ pages/api/request-password-reset/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import axios from '@lib/server/axios';
 import { Environment } from '@lib/server/environment';
+import { getDeviceType } from '@lib/utils/browser';
+import { extractIP } from '@lib/utils/location';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -8,11 +10,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log('Unsupported method');
     res.status(405).json({ message: 'Method not supported' });
   }
+
+  const forwardAddr = String(
+    req.headers['x-forwarded-for'] ?? '127.0.0.1'
+  ).split(',')[0]; // ::ffff:127.0.0.1
+  const userIp = extractIP(forwardAddr);
+  const userAgent = req.headers['user-agent'] ?? '';
+
   try {
     const response = await axios.post(
       `${Environment.API_URL}/v1/pub/request_forgot_password`,
       {
         payload: req.body.payload
+      },
+      {
+        headers: {
+          'pb-device': getDeviceType(userAgent),
+          'pb-user-ip': userIp
+        }
       }
     );
 
